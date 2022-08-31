6a558826789c58aaa3e4922aa9b4adc44a3c33a9
Yasiru Navoda Jayasekara <yasirunavoda518@gmail.com>
2022-08-31 23:01:06 +0530

    Registration implementation done

---
+++ frontend/src/components/views/RegisterPage/RegisterPage.js
@@ -0,0 +1,49 @@
+import React, {useState} from "react";
+import {Radio } from "antd";
+import StudentReg from "./Sections/StudentRegPage";
+import LecturerReg from "./Sections/LecturerRegPage"
+
+function RegisterPage(props) {
+
+    const [regType,setRegType] = useState(0);
+
+    const onChange = e => {
+        console.log('radio checked', e.target.value);
+        setRegType(e.target.value);
+    };
+
+    return (
+        <div className="app" style={{ width: '75%', margin: '3rem auto' }}>
+            <h2>Sign Up</h2>
+            <Radio.Group
+                /*options={options}*/
+                onChange={onChange}
+                // value={regType}
+                optionType="button"
+                defaultValue="0"
+                buttonStyle="solid"
+            >
+                <Radio.Button value="0">Student</Radio.Button>
+                <Radio.Button value="1">
+                    Lecturer
+                </Radio.Button>
+            </Radio.Group>
+
+            { regType=="0"  ?
+                <>
+                    <StudentReg/>
+                </>
+
+                : null }
+            { regType=="1"  ?
+                <>
+                    <LecturerReg/>
+                </>
+
+                : null }
+
+        </div>
+    );
+};
+
+export default RegisterPage
